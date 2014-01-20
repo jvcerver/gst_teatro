6bd9dc0b0acd23f1b4cba9f19092e593eb46f82a
caherfe <caherfe@hotmail.com>
2014-01-20 12:41:10 +0100

    Todo al día

--- Web/php/headerAndFooter.php
+++ Web/php/headerAndFooter.php
@@ -35,6 +35,6 @@ function cabecera(){
 }
 function pieDePagina(){
 	?>
-	<h3>© Copyright DAM all rights reserved</h3><a href="php/admin.php"><img id="imagenCandado" src="imagenes/candado.png"/></div></a>	
+	<h3>&copy; Copyright DAM all rights reserved</h3><a href="../php/admin.php"><img id="imagenCandado" src="../imagenes/candado.png"/></div></a>	
 <?php }
 ?>
